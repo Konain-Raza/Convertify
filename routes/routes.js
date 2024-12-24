3b7a8f23091080ab819aa92ccf1cba75c8f65db6
Konain Raza <konainraza105@gmail.com>
2024-12-24 16:49:02 +0500

    added ip in router

--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,14 @@ const router = express.Router();
 import { converter } from '../controllers/controllers.js';
 
 router.post("/", converter); 
+
 router.get("/getip", (req, res) => {
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || req.headers["x-real-ip"] || "127.0.0.1";
+    let ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    if (ip === "::1" || ip === "127.0.0.1") {
+        ip = "8.8.8.8";
+    }
     console.log("Detected IP:", ip);
-    const ipForGeo = ip === "::1" || ip === "127.0.0.1" ? "8.8.8.8" : ip;
-    console.log(ipForGeo);
-    res.json({
-      ip: ipForGeo
-    })
-
-  });
-
+    res.json({ ip });
+});
 
 export default router;
