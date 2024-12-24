74bef8f99b0f18dbb2801862e066bdd275ee63ac
Konain Raza <131591948+Konain-Raza@users.noreply.github.com>
2024-12-24 19:44:46 +0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import geoip from '../var/task/node_modules/geoip-lite/data';
+import geoip from '../node_modules/geoip-lite/data';
 
 
 const router = express.Router();  
